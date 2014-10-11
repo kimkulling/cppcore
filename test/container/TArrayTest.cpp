a7ffcb61f747a8bbefe7c711cde4c7c273745192
Kim Kulling <kim.kulling@googlemail.com>
2014-10-11 11:05:31 +0200

    update add unittest for ContainerClear.
    Signed-off-by: Kim Kulling <kim.kulling@googlemail.com>

--- test/container/TArrayTest.cpp
+++ test/container/TArrayTest.cpp
@@ -1,4 +1,29 @@
+/*
+-------------------------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2014 Kim Kulling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-------------------------------------------------------------------------------------------------
+*/
 #include <cppcore/Container/TArray.h>
+#include <cppcore/CPPCoreCommon.h>
 
 #include "gtest/gtest.h"
 
@@ -190,6 +215,22 @@ TEST_F( TArrayTest, findTest )	{
     TArray<float>::Iterator it = arrayInstance.find( 1.0f );
 	EXPECT_NE( it, arrayInstance.end() );
 	EXPECT_EQ( *it, 1.0f );
+}
+
+//---------------------------------------------------------------------------------------------
+TEST_F( TArrayTest, ContainerClearTest ) {
+    TArray<float*> arrayInstance;
+    ContainerClear( arrayInstance );
+    EXPECT_TRUE( arrayInstance.isEmpty() );
+
+    arrayInstance.add( new float( 0.0f ) );
+    arrayInstance.add( new float( 1.0f ) );
+    arrayInstance.add( new float( 2.0f ) );
+
+    EXPECT_EQ( arrayInstance.size(), 3 );
+    ContainerClear( arrayInstance );
+    EXPECT_EQ( arrayInstance.size(), 0 );
+    EXPECT_TRUE( arrayInstance.isEmpty() );
 }
 
 //---------------------------------------------------------------------------------------------
