3767267b861a81ab9c5b6057b116793448b8d5e0
kimkulling <kim.kulling@googlemail.com>
2019-08-09 11:49:29 +0200

    another fix for cmake generation.

--- .travis.sh
+++ .travis.sh
@@ -13,16 +13,19 @@ function generate() {
         OPTIONS="$OPTIONS -DCPPCORE_UBSAN=OFF"
     fi
 
+    cd build
     cmake -G "Unix Makefiles" $OPTIONS
 }
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  if [ $ANALYZE = "ON" ] ; then
+  if [ "$ANALYZE" = "ON" ] ; then
     if [ "$CC" = "clang" ]; then
+        cd build
+        scan-build cmake -G "Unix Makefiles"
         scan-build --status-bugs make -j4
     fi
   else
     generate \
-    && cd build && make -j4 && cd bin && ./cppcore_unittest
+    && make -j4 && cd bin && ./cppcore_unittest
   fi
 fi
