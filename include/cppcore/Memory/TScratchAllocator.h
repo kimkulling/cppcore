c688e34470d3e22d82041362b64ba2c2b2207fd9
Kim Kulling <kimkulling@users.noreply.github.com>
2024-10-29 21:01:34 +0100

    Update TScratchAllocator.h

--- include/cppcore/Memory/TScratchAllocator.h
+++ include/cppcore/Memory/TScratchAllocator.h
@@ -44,15 +44,6 @@ public:
     /// @brief  The default class constructor.
     TScratchAllocator();
     
-    /// @brief The move constructor
-    /// @param other Will be moved to
-    TScratchAllocator(TScratchAllocator&& other) noexcept;
-
-    /// @brief The move operator
-    /// @param other Will be moved to
-    /// @return instance to moved to.
-    TScratchAllocator& operator = (TScratchAllocator&& other) noexcept;
-
     /// @brief  The class constructor with the pool size.
     /// @param[in]  numItems    The buffer size.
     explicit TScratchAllocator(size_t numItems);
@@ -165,29 +156,6 @@ inline size_t TScratchAllocator<T>::freeMem() const {
     return (mSize - mIndex);
 }
 
-template<class T>
-inline TScratchAllocator(TScratchAllocator&& other) noexcept :
-    mBlock(other.mBlock),
-    mSize(other.mSize),
-    mIndex(other.mIndex) {
-    other.mBlock = nullptr;
-    other.mSize = 0;
-    other.mIndex = 0;
-}
-
-template<class T>
-inline TScratchAllocator& operator=(TScratchAllocator&& other) noexcept {
-    if (this != &other) {
-        clear();
-        mBlock = other.mBlock;
-        mSize = other.mSize;
-        mIndex = other.mIndex;
-        other.mBlock = nullptr;
-        other.mSize = 0;
-        other.mIndex = 0;
-    }
-    return *this;
-}
 using ScratchAllocator = TScratchAllocator<char>;
 
 } // namespace cppcore
