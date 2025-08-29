fd5d65bb5f15b0bf022ed504813d0e704698c91b
Kim Kulling <kimkulling@users.noreply.github.com>
2025-08-29 20:58:48 +0200

    Update DateTimeTest.cpp

--- test/common/DateTimeTest.cpp
+++ test/common/DateTimeTest.cpp
@@ -7,5 +7,16 @@ class DateTimeTest : public testing::Test {};
 
 TEST_F(DateTimeTest, CreateTest) {
     DateTime dt;
-    EXPECT_EQ(dt.year, 2025);
+    const time_t ts = ::time(nullptr);
+    tm now{};
+#if defined(_WIN32)
+    ::localtime_s(&now, &ts);
++#else
+    ::localtime_r(&ts, &now);
++#endif
+    EXPECT_EQ(dt.year, static_cast<uint32_t>(now.tm_year + 1900));
+    EXPECT_GE(dt.month, 1u);
+    EXPECT_LE(dt.month, 12u);
+    EXPECT_GE(dt.day, 1u);
+    EXPECT_LE(dt.day, 31u);
 }
