bcfe0691eacf2442280b1ca118a4485c84b2c394
Kim Kulling <kimkulling@users.noreply.github.com>
2026-05-13 11:08:30 +0200

    Update copyright year in TStackAllocator.h
    Updated copyright year from 2025 to 2026.

--- include/cppcore/Memory/TStackAllocator.h
+++ include/cppcore/Memory/TStackAllocator.h
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------------------------
 The MIT License (MIT)
 
-Copyright (c) 2014-2025 Kim Kulling
+Copyright (c) 2014-2026 Kim Kulling
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -31,6 +31,7 @@ namespace cppcore {
 //-------------------------------------------------------------------------------------------------
 ///	@class		TStackAllocator
 ///	@ingroup	CPPCore
+/// @tparam     Type for the allocation
 ///
 ///	@brief  This class implements a simple stack-based allocation scheme.
 /// Initially you have to define its size. Each allocation will be done from this initially created
